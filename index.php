a049f7a704058ec0626581ad0821586d1dd99abf
sven-eliasen <jo.pettersen@pm.me>
2021-04-05 20:35:56 +0200

    var_dump tests

--- index.php
+++ index.php
@@ -3,31 +3,8 @@
 require_once "api/Database.php";
 require_once "api/Crud/Brand/Brand.php";
 
-header("Content-Type: application/json");
+//header("Content-Type: application/json");
 
-// Connect to database
-$db = new Database();
-
-$brand = new Brand($db->conn);
-
-//echo json_encode($brand->readAll(array("brandName")));
-
-/*$brand->update([
-    3,
-    "brandName" => "Microsoft",
-    "logo" => "microsoft.png"
-    ]
-);*/
-
-$brand->delete(5);
-
-//echo json_encode($brand->readAll(array()));
-
-// for later
-/*if (empty($_GET["path"])) {
-    echo json_encode(array("errors" => [
-            "No path indicated !"
-        ])
-    );
-}*/
+var_dump($_POST);
+var_dump($_GET);
 
