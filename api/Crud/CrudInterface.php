0762de7bc6d36183b691b690943207df1633ac88
sven-eliasen <jo.pettersen@pm.me>
2021-04-09 14:14:20 +0200

    update crud phpdoc

--- api/Crud/CrudInterface.php
+++ api/Crud/CrudInterface.php
@@ -7,33 +7,52 @@ interface CrudInterface
      *
      * args is the list of attributes you want to read
      * if empty, return all of them
+     *
+     * @param array $args
+     * @return array
+     */
+    public function readAll(array $args): array;
+
+    /**
+     * Read attribute(s) of an item
+     *
+     * args[0]  : the item id you want to read from
+     * args > 0 : is the list of attributes you want to read
+     * if empty, return all of them
+     *
+     * @param array $args
+     * @return array
+     */
+    public function read(array $args): array;
+
+    /**
+     * Create an item
+     *
+     * args is the list of attributes you want to fill
+     * return error if empty or incomplete
+     *
      * @param array $args
      */
-    public function readAll(array $args);
-
-    /// Read attribute(s) of an item
-    ///
-    /// args[0]  : the item id you want to read from
-    /// args > 0 : is the list of attributes you want to read
-    /// if empty, return all of them
-    public function read(array $args);
-
-    /// Create an item
-    ///
-    /// args is the list of attributes you want to fill
-    /// return error if empty or incomplete
     public function create(array $args);
 
-    /// Update attribute(s) of an item
-    ///
-    /// args[0]  : the item id you want to update from
-    /// args > 0 : is the list of attributes you want to update
-    /// return error if empty or incomplete
+    /**
+     * Update attribute(s) of an item
+     *
+     * args[0]  : the item id you want to update from
+     * args > 0 : is the list of attributes you want to update
+     * return error if empty or incomplete
+     *
+     * @param array $args
+     */
     public function update(array $args);
 
-    /// Delete item(s)
-    ///
-    /// args is the list of item(s) id you want to delete
-    /// if empty, delete all of them (just kidding, it return an error)
+    /**
+     * Delete item
+     *
+     * args is the list of item(s) id you want to delete
+     * if empty, delete all of them (just kidding, it return an error)
+     *
+     * @param int $item
+     */
     public function delete(int $item);
 }
