dbe55c5c89032010e313016240f813d63b74fa6e
Antoine <antoine.lorin2@gmail.com>
2021-05-08 15:55:31 +0200

    back(product/cart): remove or and product to cart

--- src/Crud/Handler/HandlerProduct.php
+++ src/Crud/Handler/HandlerProduct.php
@@ -2,6 +2,36 @@
 
 class HandlerProduct extends Handler
 {
+
+    public function route(array $pathArr): array
+    {
+        if ($pathArr[1] === "AddToCart") return $this->addToCart($pathArr);
+        if ($pathArr[1] === "RemoveFromCart") return $this->removeFromCart($pathArr);
+        return parent::route($pathArr);
+    }
+
+
+    protected function addToCart(): array
+    {
+        $db = new Database();
+        $object = new Cart($db->conn);
+        $handler = new HandlerCart($object);
+
+        $result = $handler->object->where(["idUser" => $_SESSION["id"]]);
+        $cart = end($result);
+
+        $this->object->update(["id" => $_POST["id"], "idCart" => $cart["idCart"]]);
+
+        return array("id" =>  $_POST["id"]);
+    }
+
+    protected function removeFromCart(): array
+    {
+        $this->object->update(["id" => $_POST["id"], "idCart" => null]);
+
+        return array("id" =>  $_POST["id"]);
+    }
+
     protected function readAll(array $pathArr): array
     {
         $result = parent::readAll($pathArr);
