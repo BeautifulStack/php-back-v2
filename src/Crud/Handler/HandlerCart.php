e1aed09eff561951d8ff17034181b78882873324
Antoine <antoine.lorin2@gmail.com>
2021-05-15 15:17:34 +0200

    get price from cart

--- src/Crud/Handler/HandlerCart.php
+++ src/Crud/Handler/HandlerCart.php
@@ -18,19 +18,53 @@ class HandlerCart extends Handler
     public function route(array $pathArr): array
     {
         if ($pathArr[1] === "Content") return $this->getCart($pathArr);
+        if ($pathArr[1] === "Price") return $this->getPrice($pathArr);
+
         return parent::route($pathArr);
+    }
+
+    protected function getProducts(): array
+    {
+
+        $db = new Database();
+        $object = new Product($db->conn);
+        $objectCart = new Cart($db->conn);
+        $handlerCart = new HandlerCart($objectCart);
+        $res = $handlerCart->object->where(["idUser" => $_SESSION["id"]]);
+        $res = end($res);
+        $cartId = $res["idCart"];
+
+        $handler = new HandlerProduct($object);
+        $results = $handler->object->where(["idCart" => $cartId]);
+        return $results;
     }
 
     protected function getCart(): array
 
     {
-        // Ici j'aurai bien utiliser la DB de cart à la place mais elle est en protected
+        return array("content" => $this->getProducts());
+    }
+
+    protected function getPrice(): array
+    {
         $db = new Database();
-        $object = new Product($db->conn);
+        $objectCart = new Cart($db->conn);
+        $handlerCart = new HandlerCart($objectCart);
+        $res = $handlerCart->object->where(["idUser" => $_SESSION["id"]]);
+        $res = end($res);
+        $cartId = $res["idCart"];
 
-        $handler = new HandlerProduct($object);
-        $results = $handler->object->where(["idCart" => $_GET['id']]);
+        $query = "SELECT * FROM product INNER JOIN product_model ON product.idModel = product_model.idModel WHERE idCart = ?";
+        $query = $db->conn->prepare($query);
+        $query->execute([$cartId]);
 
-        return array("content" => $results);
+        $res = $query->fetchAll();
+        $total = 0;
+        foreach ($res as $row) {
+            $total += $row["officialPrice"] * 0.66;
+        }
+
+        $total = round($total);
+        return array("price" => $total);
     }
 }
