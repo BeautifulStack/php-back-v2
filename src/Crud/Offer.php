df0b7687a27881b43c4e42d632bf221ea59b7d18
Antoine <antoine.lorin2@gmail.com>
2021-05-16 11:26:14 +0200

    back(offer): update Offer creation

--- src/Crud/Offer.php
+++ src/Crud/Offer.php
@@ -22,18 +22,21 @@ class Offer extends CrudClass implements CrudInterface
 
     public function create(array $args)
     {
-        $args = $this->check_attributes_create($args, $this->attributes, $this->key, ["dateOffer", "counterOffer"]);
+
+        $args = $this->check_attributes_create($args, $this->attributes, $this->key, ["dateOffer", "counterOffer", "isAccepted", "idUser"]);
 
         if (!array_key_exists("counterOffer", $args)) $args["counterOffer"] = NULL;
 
+
         $query = $this->conn->prepare("INSERT INTO offer(price, conditionOffer, isAccepted, idModel, counterOffer, idUser) VALUES (?, ?, ?, ?, ?, ?)");
+
         $query->execute([
             $args["price"],
             $args["conditionOffer"],
-            $args["isAccepted"],
+            0,
             $args["idModel"],
             $args["counterOffer"],
-            $args["idUser"]
+            $_SESSION["id"]
         ]);
 
         $query = $this->conn->query("SELECT LAST_INSERT_ID() as id");
