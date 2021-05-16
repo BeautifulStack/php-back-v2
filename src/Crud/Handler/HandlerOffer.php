b98c4646e6fa35f4dac5dc219f3a942ea69329e5
Antoine <antoine.lorin2@gmail.com>
2021-05-16 22:37:52 +0200

    wip

--- src/Crud/Handler/HandlerOffer.php
+++ src/Crud/Handler/HandlerOffer.php
@@ -8,12 +8,57 @@ class HandlerOffer extends Handler
     {
         if ($pathArr[1] === "History") return $this->getHistory($pathArr);
         if ($pathArr[1] === "All") return $this->getOffer($pathArr);
+        if ($pathArr[1] === "CounterOffer") return $this->CounterOffer($pathArr);
         return parent::route($pathArr);
+    }
+
+    protected function CounterOffer(): array
+    {
+        $args = [];
+        $args["price"] = $_POST["price"];
+        $args["conditionOffer"] = $_POST["conditionOffer"];
+        $args["idModel"] = $_POST["idModel"];
+
+        $needed = ["price", "conditionOffer", "idModel"];
+        foreach ($needed as $arg) {
+            if (!array_key_exists($arg, $_POST)) {
+                return array("errros" => "missing field $arg");
+                exit();
+            }
+        }
+
+        if (!isset($_SESSION["id"])) {
+            return array("errros" => "You must be logged In");
+            exit();
+        }
+
+        $created = $this->object->create($args);
+        $new_id = $created["id"];
+
+        $this->object->update(["id" => $_POST['id'], "counterOffer" => $new_id, "isAccepted" => 2]);
+        return array("id" => $new_id);
     }
 
     protected function getOffer(): array
     {
-        return array("content" => $this->object->where(["idUser" => $_SESSION['id']]));
+        $offers = $this->object->where(["idUser" => $_SESSION['id']]);
+        $tmpOffers = array();
+        foreach ($offers as $offer) {
+
+            $offer["idUser"] = "You";
+            array_push($tmpOffers, $offer);
+        }
+
+        $newArray = array();
+        foreach ($tmpOffers as $offer) {
+
+            if ($offer["counterOffer"] != NULL) {
+                $offer["counterOffer"] = $this->getHistoryById($offer["counterOffer"]);
+                // var_dump($offer);
+            }
+            array_push($newArray, $offer);
+        }
+        return array("content" => $newArray);
     }
 
     public function readAll(array $pathArr): array
@@ -30,18 +75,34 @@ class HandlerOffer extends Handler
         }
     }
 
-    protected function getHistory(): array
-
+    protected function getHistoryById(int $id): array
     {
+        $db = new Database();
+        $handler = new User($db->conn);
+        $users = new HandlerUser($handler);
+
         $result = [];
-        $currentId = $_GET['id'];
+
         while (True) {
-            $res = $this->object->where(["idOffer" => $currentId])[0];
+            $res = $this->object->where(["idOffer" => $id])[0];
+            $user = $users->object->where(["idUser" => $res["idUser"]])[0];
+
+            if ($user["isAdmin"] == "1") {
+                $res["idUser"] = "Us";
+            } else {
+                $res["idUser"] = "You";
+            }
             array_push($result, $res);
             if (!isset($res["counterOffer"])) break;
-            $currentId = $res["counterOffer"];
+            $id = $res["counterOffer"];
         }
 
         return $result;
+    }
+
+    protected function getHistory(): array
+    {
+        $currentId = $_GET['id'];
+        return $this->getHistoryById($currentId);
     }
 }
